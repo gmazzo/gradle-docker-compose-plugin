321a76843fcdbb112653e2478654cfa1a8f44cf0
Guillermo Mazzola <gmazzo65@gmail.com>
2023-09-13 10:19:28 +0200

    Added `publishToMavenLocal` task

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,17 @@ plugins {
 val pluginBuild = gradle.includedBuild("plugin")
 
 tasks.check {
-    dependsOn(pluginBuild.task(":check"))
+    dependsOn(pluginBuild.task(":$name"))
 }
 
 tasks.build {
-    dependsOn(pluginBuild.task(":build"))
+    dependsOn(pluginBuild.task(":$name"))
 }
 
 tasks.register("publish") {
-    dependsOn(pluginBuild.task(":publish"))
+    dependsOn(pluginBuild.task(":$name"))
+}
+
+tasks.register("publishToMavenLocal") {
+    dependsOn(pluginBuild.task(":$name"))
 }
