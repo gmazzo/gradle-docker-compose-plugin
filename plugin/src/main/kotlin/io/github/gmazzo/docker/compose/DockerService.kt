775ac22cce382b65b856a267f76ab20e263cd348
Guillermo Mazzola <gmazzo65@gmail.com>
2023-10-02 14:20:14 +0200

    Logs less verbose

--- plugin/src/main/kotlin/io/github/gmazzo/docker/compose/DockerService.kt
+++ plugin/src/main/kotlin/io/github/gmazzo/docker/compose/DockerService.kt
@@ -1,14 +1,14 @@
 package io.github.gmazzo.docker.compose
 
-import org.gradle.api.Action
 import org.gradle.api.file.FileCollection
 import org.gradle.api.logging.Logging
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
 import org.gradle.process.ExecOperations
-import org.gradle.process.ExecResult
-import org.gradle.process.ExecSpec
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.InputStream
+import java.nio.charset.StandardCharsets
 import javax.inject.Inject
 
 @Suppress("LeakingThis")
@@ -31,40 +31,64 @@ abstract class DockerService @Inject constructor(
 
             exec(
                 "login", server,
-                *user?.let { arrayOf("--username", it, "--password-stdin") }.orEmpty()
-            ) {
-                if (password != null) standardInput = password.byteInputStream()
-            }
+                *user?.let { arrayOf("--username", it, "--password-stdin") }.orEmpty(),
+                input = password?.byteInputStream()
+            )
         }
     }
 
     @JvmOverloads
     fun exec(
         vararg command: String,
-        action: Action<in ExecSpec>? = null,
-    ): ExecResult = execOperations.exec {
-        executable = parameters.command.get()
-        args = parameters.options.get() + command
-        action?.execute(this)
+        workingDirectory: File? = null,
+        input: InputStream? = null,
+        failNonZeroExitValue: Boolean = true,
+    ): ExecResult {
+        lateinit var commands: List<String>
+        val output = ByteArrayOutputStream()
+        val error = ByteArrayOutputStream()
+        val both = ByteArrayOutputStream()
+
+        val result = execOperations.exec {
+            executable = parameters.command.get()
+            args = parameters.options.get() + command
+            if (input != null) standardInput = input
+            if (workingDirectory != null) workingDir = workingDirectory
+            standardOutput = TeeOutputStream(output, both)
+            errorOutput = TeeOutputStream(error, both)
+            isIgnoreExitValue = true
+            commands = commandLine
+        }
+        return ExecResult(
+            command = commands,
+            exitValue = result.exitValue,
+            standardOutput = output,
+            standardError = error,
+            combinedOutput = both,
+        ).also {
+            if (failNonZeroExitValue) {
+                it.assertNormalExitValue()
+            }
+        }
     }
 
-    @JvmOverloads
     fun composeExec(
         settings: DockerComposeCreateSettings,
         vararg command: String,
-        action: Action<in ExecSpec>? = null,
-    ) = settings.workingDirectory.get().asFile.let { workingDirectory ->
+        input: InputStream? = null,
+        failNonZeroExitValue: Boolean = true,
+    ) = settings.workingDirectory.get().asFile.let { workingDir ->
         exec(
             "compose",
             "--project-name",
             settings.projectName.get(),
             "-f",
-            settings.composeFile.singleFileOrThrow.toRelativeString(workingDirectory),
+            settings.composeFile.singleFileOrThrow.toRelativeString(workingDir),
             *command,
-        ) {
-            workingDir = workingDirectory
-            action?.execute(this)
-        }
+            workingDirectory = workingDir,
+            input = input,
+            failNonZeroExitValue = failNonZeroExitValue,
+        )
     }
 
     private val FileCollection.singleFileOrThrow: File
@@ -84,5 +108,32 @@ abstract class DockerService @Inject constructor(
         }
 
     interface Params : BuildServiceParameters, DockerSettings
+
+    class ExecResult(
+        val command: List<String>,
+        val exitValue: Int,
+        standardOutput: ByteArrayOutputStream,
+        standardError: ByteArrayOutputStream,
+        combinedOutput: ByteArrayOutputStream,
+    ) {
+
+        val standardOutput by lazy { standardOutput.content }
+
+        val standardError by lazy { standardError.content }
+
+        val combinedOutput by lazy { combinedOutput.content }
+
+        private val ByteArrayOutputStream.content
+            get() = toString(StandardCharsets.UTF_8).trim()
+
+        fun assertNormalExitValue() = check(exitValue == 0) {
+            command.joinToString(
+                prefix = "Command `",
+                separator = " ",
+                postfix = "` finished with non-zero exit value $exitValue:\n$combinedOutput"
+            )
+        }
+
+    }
 
 }
