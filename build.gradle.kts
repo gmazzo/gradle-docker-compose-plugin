9bebdb67d4d713d51b6f089f4b1939759ac20629
Guillermo Mazzola <gmazzo65@gmail.com>
2023-12-18 00:47:04 +0100

    Added `reportPublications` plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,25 @@
 plugins {
-    base
+    alias(libs.plugins.publicationsReport)
 }
 
-tasks {
-    val pluginBuild = gradle.includedBuild("plugin")
+val pluginBuild = gradle.includedBuild("plugin")
 
-    sequenceOf(
-        check,
-        build,
-        register("publish"),
-        register("publishToMavenLocal"),
-    ).forEach {
-        it.configure { dependsOn(pluginBuild.task(":$name")) }
-    }
+tasks.register(LifecycleBasePlugin.BUILD_TASK_NAME) {
+    dependsOn(pluginBuild.task(":$name"))
+}
+
+tasks.register(LifecycleBasePlugin.CHECK_TASK_NAME) {
+    dependsOn(pluginBuild.task(":$name"))
+}
+
+tasks.register(PublishingPlugin.PUBLISH_LIFECYCLE_TASK_NAME) {
+    dependsOn(pluginBuild.task(":$name"))
+    finalizedBy(tasks.reportPublications)
+}
+
+tasks.register(MavenPublishPlugin.PUBLISH_LOCAL_LIFECYCLE_TASK_NAME) {
+    dependsOn(pluginBuild.task(":$name"))
+    finalizedBy(tasks.reportPublications)
 }
 
 allprojects {
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.samWithReceiver)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.gradle.pluginPublish)
+    alias(libs.plugins.publicationsReport)
     jacoco
 }
 
