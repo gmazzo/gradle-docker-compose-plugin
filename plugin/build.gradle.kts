b74dfbc74d6846a083dd67ff577f3f546b39b7c7
Guillermo Mazzola <gmazzo65@gmail.com>
2024-09-05 15:40:23 +0200

    Removed `kotlinx.datetime` dependency

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -24,7 +24,6 @@ samWithReceiver.annotation(HasImplicitReceiver::class.java.name)
 dependencies {
     compileOnly(gradleKotlinDsl())
 
-    implementation(libs.kotlin.datetime)
     implementation(libs.kotlin.serialization.json)
 
     testImplementation(gradleKotlinDsl())
